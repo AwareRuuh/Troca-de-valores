c581d5c37590fa82dd8a9b3de4d1dddc619e5a9b
Ruth <88747761+AwareRuuh@users.noreply.github.com>
2022-10-08 15:15:17 +0000

    Troca de valores.

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 class Main {
+  
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    String num1;
+    String num2;
+    String aux;
+    Scanner Ler = new Scanner(System.in);
+    num1 = "";
+    num2 = "";
+    System.out.println("- Troca de valores entre variáveis - ");
+    System.out.println("Digite sua cor fav.: ");
+    num1 = Ler.nextLine();
+    System.out.println("Digite seu número da sorte: ");
+    num2 = Ler.nextLine();
+    aux = num2;
+    num2 = num1;
+    num1 = aux;
+    System.out.println(num1 + num2);
+
   }
+
 }
